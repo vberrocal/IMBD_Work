94691616df024431539adba665b656adb58fc92a
Willy Aguirre <marti1125@gmail.com>
2014-07-01 18:08:50 -0500

    actualizacion de pc1.sql

--- pc1.sql
+++ pc1.sql
@@ -16,9 +16,8 @@ CREATE TABLE Estudiante (
     fecha_nacimiento [date] NOT NULL)   
    
 ALTER TABLE Estudiante ADD CONSTRAINT PK_Estudiante PRIMARY KEY( id_estudiante );   
-
 -- validaciones
-ALTER TABLE Estudiante ADD CONSTRAINT CK_Nombre CHECK (substring(nombres,1,1) = UPPER(substring(nombres,1,1)));
+ALTER TABLE Estudiante ADD CONSTRAINT CK_Nombre CHECK ( SUBSTRING(nombres,1,1) = UPPER(SUBSTRING(nombres,1,1)) );  
 ALTER TABLE Estudiante ADD CONSTRAINT CK_Ap_Paterno CHECK (substring(ap_paterno,1,1) = UPPER(substring(ap_paterno,1,1)));
 ALTER TABLE Estudiante ADD CONSTRAINT CK_Ap_Materno CHECK (substring(ap_materno,1,1) = UPPER(substring(ap_materno,1,1)));
 ALTER TABLE Estudiante ADD CONSTRAINT CK_Sexo CHECK (sexo = 'M' or sexo = 'F'); 
@@ -43,10 +42,10 @@ ALTER TABLE Estudiante_Curso add constraint FK__Estudiante_Curso_Estudiante
 FOREIGN KEY (id_estudiante) references Estudiante;
 
 -- validaciones
-ALTER TABLE Estudiante_Curso ADD CONSTRAINT CK_nota_final CHECK (nota_final > 0 and nota_final < 20);
+ALTER TABLE Estudiante_Curso ADD CONSTRAINT CK_nota_final CHECK (nota_final >= 0 and nota_final <= 20);
 
--- el correlativo es porque hay dos registros con PC
 CREATE TABLE Evaluacion (
+	id_evaluacion int IDENTITY(1,1) NOT NULL,
     id_curso [nvarchar](6) NOT NULL,
     id_estudiante [nvarchar](10) NOT NULL,
     tipo [char](2) NOT NULL,   
@@ -61,4 +60,29 @@ FOREIGN KEY (id_estudiante) references Estudiante;
 
 -- validaciones
 ALTER TABLE Evaluacion ADD CONSTRAINT CK_tipo CHECK (tipo = 'PC' or tipo = 'TF' or tipo = 'EB');
-ALTER TABLE Evaluacion ADD CONSTRAINT CK_nota CHECK (nota > 0 and nota < 20);
+ALTER TABLE Evaluacion ADD CONSTRAINT CK_nota CHECK (nota >= 0 and nota <= 20);
+
+-- campos de auditoria para la tabla Evaluacion
+ALTER TABLE Evaluacion ADD fecha_registro date
+ALTER TABLE Evaluacion ADD fecha_modificacion date
+
+-- triggers de insert y update
+CREATE TRIGGER InsertarFechaRegistro
+ON dbo.Evaluacion
+AFTER INSERT AS
+BEGIN
+SET NOCOUNT ON;
+update dbo.Evaluacion set fecha_registro = GETDATE() where id_evaluacion = @@IDENTITY
+END
+GO
+
+CREATE TRIGGER InsertarFechaModificacion
+ON dbo.Evaluacion
+AFTER UPDATE AS
+IF (UPDATE(id_curso) or UPDATE(id_estudiante)
+or UPDATE(tipo)or UPDATE(fecha_evaluacion) or UPDATE(nota))
+BEGIN
+SET NOCOUNT ON;
+update dbo.Evaluacion set fecha_modificacion = GETDATE() where id_evaluacion = @@IDENTITY
+END
+GO
