7e5b0013f7586939155b2ceca2b7cbd9304f69d5
Willy Aguirre <marti1125@gmail.com>
2014-07-21 00:45:03 -0500

    en progress

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE BIBLIOTECA;
+CREATE DATABASE Biblioteca;
 GO
 
-USE BIBLIOTECA;
+USE Biblioteca;
 GO
 
 -- tablas principales
@@ -16,21 +16,6 @@ email varchar(60) NULL,
 alter table Autor add constraint PK_AUTOR
 primary key (codigo);
 
-INSERT INTO Autor (nombres, email) VALUES
-('Maribel Sabana Mendoza','mary_123@gmail.com'),
-('Orlando Toribio Fuentes',NULL),
-('Rosa Toro Campos','tora@gmail.com'),
-('Alfredo Santiago Santiago','santi_314@spee.com'),
-('César Cueto Barreto','barreto@gmail.com'),
-('Joaquin Espinoza Ortega',NULL),
-('Andrés Benavides Lara',NULL),
-('Clara Ponce León','clara_ponce@gmail.com'),
-('Valeria Camacho Gutierrez',NULL),
-('Esteban Loayza Pérez',NULL),
-('Antonio Bayer Flores','anto_77@grupoarg.com'),
-('Diana Mejía Gómez',NULL),
-('Ignacio Ramirez Prieto',NULL);
-
 CREATE TABLE Editorial
 (
 codigo int IDENTITY(1,1) NOT NULL,
@@ -42,11 +27,6 @@ telefono varchar(20) NULL
 alter table Editorial add constraint PK_EDITORIAL
 primary key (codigo);
 
-INSERT INTO Editorial (nombre, direccion, telefono) VALUES
-('NAVARRETE','Av. Nicolas Piérola 351 SanLuis','5213696'),
-('NORMA','Calle Los Pinos 841 Santa Beatriz','3581299'),
-('SANTILLANA','Av. Juan Suarez 101 Lima','2315184 - 5329566');
-
 CREATE TABLE GeneroLibro
 (
 codigo int IDENTITY(1,1) NOT NULL,
@@ -55,54 +35,6 @@ nombre varchar(50) NOT NULL
 
 alter table GeneroLibro add constraint PK_ESPECIALIDADLIBRO
 primary key (codigo);
-
-INSERT INTO GeneroLibro (nombre) VALUES
-('MATEMÁTICA'),
-('COMPUTACIÓN'),
-('INGLÉS'),
-('LENGUA'),
-('LITERATURA'),
-('RELIGIÓN');
-
-CREATE TABLE EstadoCarnet
-(
-codigo int IDENTITY(1,1) NOT NULL,
-estado char(1) NOT NULL,
-denominacion varchar(30) NOT NULL
-);
-
-alter table EstadoCarnet add constraint PK_ESTADOCARNET
-primary key (codigo);
-
-INSERT INTO EstadoCarnet (estado, denominacion) VALUES
-('H','Habilitado'),
-('R','Retenido'),
-('S','Suspendido');
-
--- fin
-
--- tablas relacionadas
-
-CREATE TABLE Carnet
-(
-codigo int identity(1,1) NOT NULL,
-codigo_carnet varchar(10) NOT NULL,
-estado_carnet_codigo int NOT NULL,
-observacion varchar(100) NULL
-);
-
-alter table Carnet add constraint PK_CARNETBIBLIOTECA
-primary key (codigo);
-
-alter table Carnet add constraint FK_ESTADOCARNET
-foreign key (estado_carnet_codigo) references EstadoCarnet;
-
-
-INSERT INTO Carnet (codigo_carnet, estado_carnet_codigo, observacion) VALUES
-('20140011',1, null),
-('20140511',1, null),
-('20140711',1, null),
-('20149811',1, null);
 
 CREATE TABLE Libro
 (
@@ -123,23 +55,6 @@ foreign key (editorial_codigo) references Editorial;
 alter table Libro add constraint FK_GENEROLIBRO
 foreign key (genero_libro_codigo) references GeneroLibro;
 
-INSERT INTO Libro (titulo, registro, cantidad_paginas, editorial_codigo, genero_libro_codigo) VALUES
-('Matemática Primer Grado','2014-01-01',88,3,1),
-('Aprendiendo Inglés','2014-01-02',75,2,3),
-('La vida de Jesús','2014-01-04',50,1,6),
-('Funciones lineales','2014-01-01',53,3,1),
-('Mantenimiento de PC','2014-01-01',84,2,2),
-('CONAMAT V','2014-01-01',48,3,1),
-('¿Quién se ha llevado mi queso?','2014-01-01',45,1,5),
-('La computadora y sus partes','2014-01-01',55,1,2),
-('Oraciones al señor','2014-01-01',51,1,6),
-('CONAMAT IV','2014-01-01',45,3,1),
-('EL DELFIN','2014-01-01',34,2,2),
-('DESARROLLO WEB','2014-01-01',67,1,1),
-('EPE','2014-01-01',35,1,2),
-('DESARROLLO MOVIL','2014-01-01',34,2,2),
-('REPARACION DE PC','2014-01-01',265,1,2),
-('Casos de la vida','2014-01-01',123,1,1);
 
 CREATE TABLE LibroAutor
 (
@@ -148,21 +63,15 @@ autor_codigo int NOT NULL,
 libro_codigo int NOT NULL
 );
 
-alter table LibroAutor add constraint PK_LIBROAUTOR
+alter table LibroAutor add constraint PK_LIBRO_AUTOR
 primary key (codigo);
 
 alter table LibroAutor add constraint FK_AUTOR
 foreign key (autor_codigo) references Autor;
 
-alter table LibroAutor add constraint FK_LIBRO_LIBROAUTOR
+alter table LibroAutor add constraint FK_LIBRO_LIBRO_AUTOR
 foreign key (libro_codigo) references Libro;
 
-INSERT INTO LibroAutor (autor_codigo, libro_codigo) VALUES
-(1,1),
-(2,2),
-(3,3),
-(4,4),
-(5,5);
 
 CREATE TABLE TipoUsuario
 (
@@ -173,18 +82,13 @@ descripcion varchar(50) NOT NULL
 alter table TipoUsuario add constraint PK_TIPOUSUARIO
 primary key (codigo);
 
-INSERT INTO TipoUsuario
-(descripcion)
-VALUES
-('ADMINISTRADOR'),
-('ESTUDIANTE');
-
 CREATE TABLE Usuario
 (
 codigo int IDENTITY(1,1) NOT NULL,
 usuario varchar(45) NOT NULL,
 clave varchar(45) NOT NULL,
 nombre_completo varchar(100) NULL,
+foto varchar(100) NULL,
 tipo_usuario_codigo int NULL
 );
 
@@ -194,13 +98,6 @@ primary key (codigo);
 alter table Usuario add constraint FK_TIPO_USUARIO
 foreign key (tipo_usuario_codigo) references TipoUsuario;
 
-INSERT INTO Usuario (usuario, clave, nombre_completo, tipo_usuario_codigo) VALUES
-('vberrocal','111','',2),
-('gsotomayor','222','',2),
-('wacosta','333','',2),
-('ecayllahua','444','',2);
-
--- tiene campos de auditoria
 CREATE TABLE Prestamo
 (
 codigo int IDENTITY(1,1) NOT NULL,
@@ -210,10 +107,8 @@ fecha_prestamo date NOT NULL,
 fecha_devolucion date NOT NULL,
 devuelto bit NOT NULL,
 observacion varchar(100) NULL,
-usuario_creacion_codigo int NOT NULL,
-fecha_creacion datetime NOT NULL,
-usuario_ultima_actualizacion_codigo int NOT NULL,
-fecha_ultima_actualizacion datetime NOT NULL
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
 );
 
 alter table Prestamo add constraint PK_PRESTAMO
@@ -225,10 +120,100 @@ foreign key (usuario_codigo) references Usuario;
 alter table Prestamo add constraint FK_LIBRO_PRESTAMO
 foreign key (libro_codigo) references Libro;
 
-INSERT INTO Prestamo (usuario_codigo, libro_codigo, fecha_prestamo, fecha_devolucion,
-devuelto, observacion, usuario_creacion_codigo, fecha_creacion,
-usuario_ultima_actualizacion_codigo, fecha_ultima_actualizacion) VALUES
-(1,3,'2014-01-01','2014-01-05',1,'OBSERVACION',1,'2014-01-01',1,'2014-01-05'),
-(2,3,'2014-01-01','2014-01-04',1,'OBSERVACION',1,'2014-01-01',1,'2014-01-04'),
-(3,3,'2014-01-01','2014-01-05',1,'OBSERVACION',1,'2014-01-01',1,'2014-01-05'),
-(4,3,'2014-01-01','2014-01-03',1,'OBSERVACION',1,'2014-01-01',1,'2014-01-03');
+CREATE TABLE DetalleAutor
+(
+codigo int IDENTITY(1,1) NOT NULL,
+nacionalidad varchar(20) NULL,
+cantidad_libros int NULL,
+libro_codigo int NULL
+)
+
+alter table DetalleAutor add constraint PK_DETALLE_AUTOR
+primary key (codigo);
+
+alter table DetalleAutor add constraint FK_DETALLE_AUTOR_LIBRO
+foreign key (libro_codigo) references Libro;
+
+-- nivel de demanda cuantitativa A(Alta), M(Media), B(Baja)
+CREATE TABLE NivelDemandaLibro
+(
+codigo int IDENTITY(1,1) NOT NULL,
+nivel_demanda char(1) NULL,
+cantidad_veces int NULL,
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
+)
+
+alter table NivelDemandaLibro add constraint PK_NIVEL_DEMANDA_LIBRO
+primary key (codigo);
+
+-- [Tarea]
+-- 1
+-- trigger para calcular el numero de veces que pidio el libro y
+-- de acuerdo se pone una letra
+-- A(Alta), M(Media), B(Baja)
+-- el crear un nuevo registro en nivel demanda libro
+-- 2
+-- crear un procemiento almacenado para hacer la actualizacion automatica
+
+CREATE TABLE EspacioLectura
+(
+codigo varchar(10) NOT NULL,
+capacidad char(1) NULL,
+estado bit NULL, -- activo o no, es decir, si no esta en repación
+observacion varchar(100) NULL
+)
+
+alter table EspacioLectura add constraint PK_ESPACIO_LECTURA
+primary key (codigo);
+
+CREATE TABLE EspacioLecturaDetalle
+(
+codigo int IDENTITY(1,1) NOT NULL,
+espacio_lectura_codigo varchar(10) NOT NULL,
+usuario_codigo int NOT NULL,
+disponible bit NULL,
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
+)
+
+alter table EspacioLecturaDetalle add constraint PK_ESPACIO_LECTURA_DETALLE
+primary key (codigo);
+
+alter table EspacioLecturaDetalle add constraint FK_ESPACIO_LECTURA_DETALLE_ESPACIO_LECTURA
+foreign key (espacio_lectura_codigo) references EspacioLectura;
+
+alter table EspacioLecturaDetalle add constraint FK_ESPACIO_LECTURA_DETALLE_USUARIO
+foreign key (usuario_codigo) references Usuario;
+
+CREATE TABLE Carnet
+(
+codigo varchar(10) NOT NULL,
+usuario_codigo int NOT NULL,
+fecha_creacion datetime NULL
+);
+
+alter table Carnet add constraint PK_CARNET
+primary key (codigo);
+
+alter table Carnet add constraint FK_CARNET_USUARIO
+foreign key (usuario_codigo) references Usuario;
+
+-- Columna estado, H(Habilitado), R(Robo), S(Suspendido), P(Perdida)
+-- Agregar el check respectivo
+CREATE TABLE DetalleCarnet
+(
+codigo int identity(1,1) NOT NULL,
+codigo_carnet varchar(10) NOT NULL,
+estado char(1) NOT NULL,
+bloquear bit NOT NULL, -- si no es esta Habilitado
+observacion varchar(100) NULL,
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
+)
+
+alter table DetalleCarnet add constraint PK_DETALLE_CARNET
+primary key (codigo);
+
+alter table DetalleCarnet add constraint FK_DETALLE_CARNET_CARNET
+foreign key (codigo_carnet) references Carnet;
