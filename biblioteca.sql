da0e5d123ba487b527999e3cf6f9168a641b1891
Willy Aguirre <marti1125@gmail.com>
2014-07-28 19:42:40 -0500

    actualizacion de biblioteca.sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -9,8 +9,12 @@ GO
 CREATE TABLE Autor
 (
 idAutor int IDENTITY(1,1) NOT NULL,
-nombres varchar(50) NOT NULL,
+appPaterno varchar(50) NULL,
+appMaterno varchar(50) NULL,
+nombres varchar(50) NULL,
+nacionalidad varchar(50) NULL,
 email varchar(60) NULL,
+cantidad_libro int NULL
 );
 
 alter table Autor add constraint PK_AUTOR
@@ -19,6 +23,7 @@ primary key (idAutor);
 CREATE TABLE Editorial
 (
 idEditorial int IDENTITY(1,1) NOT NULL,
+ruc varchar(10) NOT NULL,
 nombre varchar(50) NOT NULL,
 direccion varchar(100) NULL,
 telefono varchar(20) NULL
@@ -27,63 +32,109 @@ telefono varchar(20) NULL
 alter table Editorial add constraint PK_EDITORIAL
 primary key (idEditorial);
 
-CREATE TABLE GeneroLibro
+CREATE TABLE Genero
 (
-idGeneroLibro int IDENTITY(1,1) NOT NULL,
-nombre varchar(50) NOT NULL
+idGenero int IDENTITY(1,1) NOT NULL,
+descripcion varchar(50) NOT NULL
 );
 
-alter table GeneroLibro add constraint PK_ESPECIALIDADLIBRO
-primary key (idGeneroLibro);
+alter table Genero add constraint PK_GENERO
+primary key (idGenero);
 
 CREATE TABLE Libro
 (
 idLibro int identity(1,1) NOT NULL,
+idGenero int NOT NULL,
+idEditorial int NOT NULL,
 titulo varchar(50) NOT NULL,
-cantidad_paginas int NULL
+cantidad_paginas int NULL,
+idioma varchar(50) NOT NULL
 );
 
 alter table Libro add constraint PK_LIBRO
 primary key (idLibro);
 
-CREATE TABLE DetalleLibro
+alter table Libro add constraint FK_GENERO
+foreign key (idGenero) references Genero;
+
+alter table Libro add constraint FK_EDITORIAL
+foreign key (idEditorial) references Editorial;
+
+CREATE TABLE AutorLibro
 (
-idDetalleLibro int identity(1,1) NOT NULL,
-libro_id int NOT NULL,
-editorial_id int NOT NULL,
-generolibro_id int NOT NULL,
+idAutor int NOT NULL,
+idLibro int NOT NULL,
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
+);
+
+alter table AutorLibro add constraint PK_AUTOR_LIBRO
+primary key (idAutor,idLibro);
+
+alter table AutorLibro add constraint FK_AL_AUTOR
+foreign key (idAutor) references Autor;
+
+alter table AutorLibro add constraint FK_AL_LIBRO
+foreign key (idLibro) references Libro;
+
+CREATE TABLE TipoDemanda
+(
+idTipoDemanda char(1) NOT NULL,
+descripcion varchar(50) NOT NULL
+);
+
+alter table TipoDemanda add constraint PK_TIPO_DEMANDA
+primary key (idTipoDemanda);
+
+-- nivel de demanda cuantitativa A(Alta), M(Media), B(Baja)
+CREATE TABLE NivelDemandaLibro
+(
+idNivelDemandaLibro int IDENTITY(1,1) NOT NULL,
+idLibro int NOT NULL,
+idTipoDemanda char(1) NULL,
+cantidad_veces int NULL, -- es una consulta que hace un trigger
 fecha_creacion datetime NULL,
 fecha_ultima_actualizacion datetime NULL
 )
 
-alter table DetalleLibro add constraint PK_DETALLELIBRO
-primary key (idDetalleLibro);
+alter table NivelDemandaLibro add constraint PK_NIVEL_DEMANDA_LIBRO
+primary key (idNivelDemandaLibro);
 
-alter table DetalleLibro add constraint FK_LIBRO
-foreign key (libro_id) references Libro;
+alter table NivelDemandaLibro add constraint FK_NDL_LIBRO
+foreign key (idLibro) references Libro;
 
-alter table DetalleLibro add constraint FK_EDITORIAL
-foreign key (editorial_id) references Editorial;
+alter table NivelDemandaLibro add constraint FK_NDL_TIPO_DEMANDA
+foreign key (idTipoDemanda) references TipoDemanda;
 
-alter table DetalleLibro add constraint FK_GENEROLIBRO
-foreign key (generolibro_id) references GeneroLibro;
-
-
-CREATE TABLE LibroAutor
+CREATE TABLE Distribuidor
 (
-idLibroAutor int identity(1,1) NOT NULL,
-autor_id int NOT NULL,
-libro_id int NOT NULL
+idDistribuidor int IDENTITY(1,1) NOT NULL,
+ruc varchar(10) NOT NULL,
+nombre varchar(50) NOT NULL,
+direccion varchar(100) NULL,
+telefono varchar(20) NULL
 );
 
-alter table LibroAutor add constraint PK_LIBRO_AUTOR
-primary key (idLibroAutor);
+alter table Distribuidor add constraint PK_DISTRIBUIDOR
+primary key (idDistribuidor);
 
-alter table LibroAutor add constraint FK_AUTOR
-foreign key (autor_id) references Autor;
+CREATE TABLE DistribuidorLibro
+(
+idDistribuidor int NOT NULL,
+idLibro int NOT NULL,
+fecha_creacion datetime NULL,
+fecha_ultima_actualizacion datetime NULL
+);
 
-alter table LibroAutor add constraint FK_LIBRO_LIBRO_AUTOR
-foreign key (libro_id) references Libro;
+alter table DistribuidorLibro add constraint PK_DISTRIBUIDOR_LIBRO
+primary key (idDistribuidor,idLibro);
+
+alter table DistribuidorLibro add constraint FK_DISTRIBUIDOR
+foreign key (idDistribuidor) references Distribuidor;
+
+alter table DistribuidorLibro add constraint FK_LIBRO
+foreign key (idLibro) references Libro;
+
 
 CREATE TABLE TipoUsuario
 (
@@ -97,18 +148,18 @@ primary key (idTipoUsuario);
 CREATE TABLE Usuario
 (
 idUsuario int IDENTITY(1,1) NOT NULL,
+idTipoUsuario int NOT NULL,
 usuario varchar(45) NOT NULL,
 clave varchar(45) NOT NULL,
 nombre_completo varchar(100) NULL,
-foto varchar(100) NULL,
-tipousuario_id int NULL
+foto varchar(100) NULL
 );
 
 alter table Usuario add constraint PK_USUARIO
 primary key (idUsuario);
 
 alter table Usuario add constraint FK_TIPO_USUARIO
-foreign key (tipousuario_id) references TipoUsuario;
+foreign key (idTipoUsuario) references TipoUsuario;
 
 CREATE TABLE Universidad
 (
@@ -117,100 +168,86 @@ nombre varchar(50) NOT NULL,
 direccion varchar(50) NULL
 )
 
-alter table Universidad add constraint PK_UBICACION
+alter table Universidad add constraint PK_UNIVERSIDAD
 primary key (idUniversidad);
+
+CREATE TABLE Instituto
+(
+idInstituto int IDENTITY(1,1) NOT NULL,
+nombre varchar(50) NOT NULL,
+direccion varchar(50) NULL
+)
+
+alter table Instituto add constraint PK_INSTITUTO
+primary key (idInstituto);
 
 CREATE TABLE CentroEstudioUsuario
 (
 idCentroEstudioUsuario int IDENTITY(1,1) NOT NULL,
-usuario_id int NOT NULL,
-universidad_id int NOT NULL
+idUniversidad int NOT NULL,
+idInstituto int NOT NULL
 )
 
 alter table CentroEstudioUsuario add constraint PK_CENTRO_ESTUDIO_USUARIO
 primary key (idCentroEstudioUsuario);
 
-alter table CentroEstudioUsuario add constraint FK_CENTRO_ESTUDIO_USUARIO_USUARIO
-foreign key (usuario_id) references Usuario;
+alter table CentroEstudioUsuario add constraint FK_CENTRO_ESTUDIO_UNIVERSIDAD
+foreign key (idUniversidad) references Universidad;
 
-alter table CentroEstudioUsuario add constraint FK_CENTRO_ESTUDIO_USUARIO_UNIVERSIDAD
-foreign key (universidad_id) references Universidad;
+alter table CentroEstudioUsuario add constraint FK_CENTRO_ESTUDIO_INSTITUTO
+foreign key (idInstituto) references Instituto;
 
-CREATE TABLE Prestamo
+
+-- Columna estado, H(Habilitado), R(Robo), S(Suspendido), P(Perdida)
+-- Agregar el check respectivo
+CREATE TABLE Carnet
 (
-idPrestamo int IDENTITY(1,1) NOT NULL,
-usuario_id int NOT NULL,
-libro_id int NOT NULL,
-fecha_prestamo date NOT NULL,
-fecha_devolucion date NOT NULL,
-devuelto bit NOT NULL,
+idCarnetUsuario int NOT NULL,
+estado char(1) NOT NULL,
+bloquear bit NOT NULL, -- si no es esta Habilitado
 observacion varchar(100) NULL,
 fecha_creacion datetime NULL,
 fecha_ultima_actualizacion datetime NULL
 );
 
+alter table Carnet add constraint PK_CARNET
+primary key (idCarnetUsuario);
+
+alter table Carnet add constraint FK_CARNET_USUARIO
+foreign key (idCarnetUsuario) references Usuario;
+
+CREATE TABLE Prestamo
+(
+idPrestamo int IDENTITY(1,1) NOT NULL,
+fecha_prestamo date NOT NULL,
+fecha_devolucion date NOT NULL,
+devuelto bit NOT NULL,
+observacion varchar(100) NULL,
+);
+
 alter table Prestamo add constraint PK_PRESTAMO
 primary key (idPrestamo);
 
-alter table Prestamo add constraint FK_USUARIO
-foreign key (usuario_id) references Usuario;
-
-alter table Prestamo add constraint FK_LIBRO_PRESTAMO
-foreign key (libro_id) references Libro;
-
-CREATE TABLE DetalleAutor
+CREATE TABLE PrestamoLibro
 (
-idDetalleAutor int IDENTITY(1,1) NOT NULL,
-nacionalidad varchar(20) NULL,
-cantidad_libros int NULL,
-libro_id int NULL
-)
-
-alter table DetalleAutor add constraint PK_DETALLE_AUTOR
-primary key (idDetalleAutor);
-
-alter table DetalleAutor add constraint FK_DETALLE_AUTOR_LIBRO
-foreign key (libro_id) references Libro;
-
--- nivel de demanda cuantitativa A(Alta), M(Media), B(Baja)
-CREATE TABLE NivelDemandaLibro
-(
-idNivelDemandaLibro int IDENTITY(1,1) NOT NULL,
-libro_id int NOT NULL,
-nivel_demanda char(1) NULL,
-cantidad_veces int NULL,
+idPrestamo int NOT NULL,
+idLibro int NOT NULL,
+idCarnetUsuario int NOT NULL,
 fecha_creacion datetime NULL,
 fecha_ultima_actualizacion datetime NULL
 )
 
-alter table NivelDemandaLibro add constraint PK_NIVEL_DEMANDA_LIBRO
-primary key (idNivelDemandaLibro);
+alter table PrestamoLibro add constraint PK_PRESTAMO_LIBRO
+primary key (idPrestamo,idLibro,idCarnetUsuario);
 
-alter table NivelDemandaLibro add constraint FK_NIVEL_DEMANDA_LIBRO_LIBRO
-foreign key (libro_id) references Libro;
+alter table PrestamoLibro add constraint FK_PRESTAMO_LIBRO_PRESTAMO
+foreign key (idPrestamo) references Prestamo;
 
--- [Tarea]
--- 1
--- trigger para calcular el numero de veces que pidio el libro y
--- de acuerdo se pone una letra
--- A(Alta), M(Media), B(Baja)
--- el crear un nuevo registro en nivel demanda libro
--- 2
--- crear un procemiento almacenado para hacer la actualizacion automatica
+alter table PrestamoLibro add constraint FK_PRESTAMO_LIBRO_LIBRO
+foreign key (idLibro) references Libro;
 
--- Medir la cantidad de estudiantes por universidad (progreso....)
-CREATE TABLE DemandaEstudiante
-(
-idDemandaEstudiante int IDENTITY(1,1) NOT NULL,
-centroestudiousuario_id int NOT NULL,
-cantidad int NULL, -- cantidad de estudiantes por universidad
-)
-
-alter table DemandaEstudiante add constraint PK_DEMANDA_ESTUDIANTE
-primary key (idDemandaEstudiante);
-
-alter table DemandaEstudiante add constraint FK_DEMANDA_CENTRO_ESTUDIO
-foreign key (centroestudiousuario_id) references CentroEstudioUsuario;
+alter table PrestamoLibro add constraint FK_PRESTAMO_LIBRO_CARNET
+foreign key (idCarnetUsuario) references Carnet;
 
 CREATE TABLE EspacioLectura
 (
@@ -223,56 +260,28 @@ observacion varchar(100) NULL
 alter table EspacioLectura add constraint PK_ESPACIO_LECTURA
 primary key (idEspacioLectura);
 
-CREATE TABLE EspacioLecturaDetalle
+CREATE TABLE PrestamoEspacioLectura
 (
-idEspLectDetalle int IDENTITY(1,1) NOT NULL,
-espaciolectura_id varchar(10) NOT NULL,
-usuario_id int NOT NULL,
+idPrestEspLectura int IDENTITY(1,1) NOT NULL,
+idEspacioLectura varchar(10) NOT NULL,
+idCarnetUsuario int NOT NULL,
 disponible bit NULL,
+fecha datetime NULL,
+hora_inicio datetime NULL,
+hora_fin datetime NULL,
 fecha_creacion datetime NULL,
 fecha_ultima_actualizacion datetime NULL
 )
 
-alter table EspacioLecturaDetalle add constraint PK_ESPACIO_LECTURA_DETALLE
-primary key (idEspLectDetalle);
+alter table PrestamoEspacioLectura add constraint PK_PRESTAMO_ESPACIO_LECTURA
+primary key (idPrestEspLectura);
 
-alter table EspacioLecturaDetalle add constraint FK_ESPACIO_LECTURA_DETALLE_ESPACIO_LECTURA
-foreign key (espaciolectura_id) references EspacioLectura;
+alter table PrestamoEspacioLectura add constraint FK_PRESTAMO_ESPACIO_LECTURA_ESPACIO_LECTURA
+foreign key (idEspacioLectura) references EspacioLectura;
 
-alter table EspacioLecturaDetalle add constraint FK_ESPACIO_LECTURA_DETALLE_USUARIO
-foreign key (usuario_id) references Usuario;
+alter table PrestamoEspacioLectura add constraint FK_ESPACIO_LECTURA_CARNET
+foreign key (idCarnetUsuario) references Carnet;
 
-CREATE TABLE Carnet
-(
-idCarnet varchar(10) NOT NULL,
-usuario_id int NOT NULL,
-fecha_creacion datetime NULL
-);
-
-alter table Carnet add constraint PK_CARNET
-primary key (idCarnet);
-
-alter table Carnet add constraint FK_CARNET_USUARIO
-foreign key (usuario_id) references Usuario;
-
--- Columna estado, H(Habilitado), R(Robo), S(Suspendido), P(Perdida)
--- Agregar el check respectivo
-CREATE TABLE DetalleCarnet
-(
-idDetalleCarnet int identity(1,1) NOT NULL,
-carnet_id varchar(10) NOT NULL,
-estado char(1) NOT NULL,
-bloquear bit NOT NULL, -- si no es esta Habilitado
-observacion varchar(100) NULL,
-fecha_creacion datetime NULL,
-fecha_ultima_actualizacion datetime NULL
-)
-
-alter table DetalleCarnet add constraint PK_DETALLE_CARNET
-primary key (idDetalleCarnet);
-
-alter table DetalleCarnet add constraint FK_DETALLE_CARNET_CARNET
-foreign key (carnet_id) references Carnet;
 
 CREATE TABLE TipoRequerimiento
 (
@@ -286,10 +295,10 @@ primary key (idTipReq);
 CREATE TABLE HistorialRequerimiento
 (
 idHistReq int IDENTITY(1,1) NOT NULL,
-tipreq_id int NOT NULL,
-prestamo_id int NOT NULL,
-detallelibro_id int NOT NULL,
-espaciolecturadetalle_id int NOT NULL,
+idTipReq int NOT NULL,
+idPrestamo int NOT NULL,
+idLibro int NOT NULL,
+idPrestEspLectura int NOT NULL,
 fecha_creacion datetime NULL,
 fecha_ultima_actualizacion datetime NULL
 )
@@ -298,13 +307,13 @@ alter table HistorialRequerimiento add constraint PK_HISTORIAL_REQUERIMIENTO
 primary key (idHistReq);
 
 alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_TIP_REQ
-foreign key (tipreq_id) references TipoRequerimiento;
+foreign key (idTipReq) references TipoRequerimiento;
 
-alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_PRESTAMO
-foreign key (prestamo_id) references Prestamo;
+alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_PRESTAMO_LIBRO
+foreign key (idPrestamo) references Prestamo;
 
-alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_DETALLE_LIBRO
-foreign key (detallelibro_id) references DetalleLibro;
+alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_LIBRO
+foreign key (idLibro) references Libro;
 
-alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_ESPACIO_LECTURA_DETALLE
-foreign key (espaciolecturadetalle_id) references EspacioLecturaDetalle;
+alter table HistorialRequerimiento add constraint FK_HISTORIAL_REQUERIMIENTO_PRESTAMO_ESPACIO_LECTURA
+foreign key (idPrestEspLectura) references PrestamoEspacioLectura;
