e9573b6e1dd4989a9ab31cb65eb1bea7695eae0a
Willy Aguirre <marti1125@gmail.com>
2014-08-01 22:58:54 -0500

    se agrega backup_biblioteca.sql

---
+++ backup_biblioteca.sql
@@ -0,0 +1,17 @@
+--script
+
+insert into Editorial values
+('ASOCIACIÓN EDITORIAL BRUÑO','Sr. Augusto Federico Díaz Tineo','Av. Arica 751. Breña', '4244134 / 4237890'),
+('CENTAURO EDICIONES S.A.C.','Sr. Óscar Gustavo Zevallos García','Jr. Luis Varela y Orbegoso 565.', '4440900'),
+('EDICIONES SM S.A.C.','Guillermo Alejandro Rivas','Calle Micaela Bastidas 125. San Isidro.', '6148900 / 6148904'),
+('EDITORIAL COVEÑAS S.A.C','Manuel Coveñas Naquiche','Jr. Las Verdolagas No. 199 Urb. Micaela Bastidas. Los Olivos.', '521-0949 / 4867957'),
+('EDITORIAL SAN MARCOS E.I.R.L.','Sr. Anibal Jesús Paredes Galván','Jr. Dávalos Lisson 135. Lima.', '3311522 / 3311535'),
+('EMPRESA EDITORA MACRO E.I.R.L.','Sra. Isabel Rosario Ramos','Av. Paseo de la República 5613. Miraflores.', '7199701 / 7199702'),
+('GRUPO EDITORIAL NORMA S.A.C.','Álvaro Ruíz','Av. Canaval y Moreyra 345. San Isidro.', '7103000'),
+('LIBRERÍA PERUANO BRITÁNICA S.R.L.','Franco Ramírez Reyes','Av. Uiversitaria Mz A Lt 5. Urb. La Libertad. Los Olivos','531-7712 -2413768'),
+('MACMILLAN PUBLISHERS S.A.','Sr. Javier Lombardi Wilson','Calle Manuel Tovar 252. Miraflores.','4410311'),
+('MEGA EDITORES S.A.C.','Sr. Mario Jesús Minaya García','Calle Río Majes Nº246.Parque San Martín-Ruinas Puruchuco. Ate.','4626221 / 4093030'),
+('NUEVAS TÉCNICAS EDUCATIVAS S.A.C. – NUTESA','Sr. Enrique Cappelletti Sotomayor','Av. República de Panamá 2197, 2º piso – Of. 2B. La Victoria.','4721712 / 2058642'),
+('PEARSON EDUCACIÓN DEL PERÚ S.A.C.','Sr. Eduardo Guzmán Barro','Av. Javier Prado Este 4491. Surco.','4372010 / 4372015'),
+('SANTILLANA S.A.','Sr. Javier Laría Quincoces','Av. Primavera 2160. Surco.','3134000'),
+('SPECIAL BOOKS SERVICES S.A.','Sra. PaulaTapia Álvarez Gerente ','Av. Angamos Oeste 301. Miraflores.','206-4900');
